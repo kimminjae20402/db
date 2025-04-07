2abe3e228a2a0c6625fc49ca80628f1b804439a9
kimminjae20402 <nan377399@gmail.com>
2025-04-07 11:54:38 +0900

    Add files via upload

---
+++ 20250407.sql
@@ -0,0 +1,7 @@
+--¹®Á¦1
+select*from student;
+select name ||' ''s ID :'||ID||', weight is '||weight||'kg' from student;
+select*from emp;
+select empno, ename, hiredate from emp where hiredate LIke '%80%'
+select empno, ename, hiredate from emp where hiredate LIke '%80%'
+Union;
